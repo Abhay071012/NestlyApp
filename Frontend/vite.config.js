13e806af8b7a6e4161c2393a934ccb8eb4d0aef4
Abhay071012 <abhaypb122005@gmail.com>
2026-09-21 21:33:15 +0530

    add backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: ' https://nestly-c7s3.onrender.com',
         changeOrigin: true,
         secure: false,
       }
